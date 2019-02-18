85ee67f2fec3b4ae362a272a716061669ea968a9
chrisnager <cnager@gmail.com>
2019-02-18 12:49:53 -0500

    Add theme

---
+++ index.js
@@ -0,0 +1,185 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+exports.default = {
+  'code[class*="language-"]': {
+    color: '#d6deeb',
+    fontFamily: "Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+    textAlign: 'left',
+    whiteSpace: 'pre',
+    wordSpacing: 'normal',
+    wordBreak: 'normal',
+    wordWrap: 'normal',
+    lineHeight: '1.5',
+    MozTabSize: '4',
+    OTabSize: '4',
+    tabSize: '4',
+    WebkitHyphens: 'none',
+    MozHyphens: 'none',
+    msHyphens: 'none',
+    hyphens: 'none',
+  },
+  'pre[class*="language-"]': {
+    color: '#d6deeb',
+    background: '#011627',
+    fontFamily: "Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+    textAlign: 'left',
+    whiteSpace: 'pre',
+    wordSpacing: 'normal',
+    wordBreak: 'normal',
+    wordWrap: 'normal',
+    lineHeight: '1.5',
+    MozTabSize: '4',
+    OTabSize: '4',
+    tabSize: '4',
+    WebkitHyphens: 'none',
+    MozHyphens: 'none',
+    msHyphens: 'none',
+    hyphens: 'none',
+    padding: '1em',
+    margin: '0.5em 0',
+    overflow: 'auto',
+  },
+  ':not(pre) > code[class*="language-"]': {
+    background: '#011627',
+    padding: '.1em',
+    borderRadius: '.3em',
+    whiteSpace: 'normal',
+  },
+  comment: {
+    color: 'rgb(99, 119, 119)',
+    fontStyle: 'italic',
+  },
+  prolog: {
+    color: 'rgb(99, 119, 119)',
+    fontStyle: 'italic',
+  },
+  doctype: {
+    color: 'rgb(199, 146, 234)',
+    fontStyle: 'italic',
+  },
+  cdata: {
+    color: 'rgb(99, 119, 119)',
+    fontStyle: 'italic',
+  },
+  punctuation: {
+    color: 'rgb(199, 146, 234)',
+  },
+  '.namespace': {
+    color: 'rgb(178, 204, 214)',
+  },
+  property: {
+    color: 'rgb(128, 203, 196)',
+  },
+  tag: {
+    color: 'rgb(127, 219, 202)',
+  },
+  constant: {
+    color: 'rgb(130, 170, 255)',
+  },
+  symbol: {
+    color: 'rgb(128, 203, 196)',
+  },
+  deleted: {
+    color: 'rgba(239, 83, 80, 0.56)',
+    fontStyle: 'italic',
+  },
+  boolean: {
+    color: 'rgb(255, 88, 116)',
+  },
+  number: {
+    color: 'rgb(247, 140, 108)',
+  },
+  selector: {
+    color: 'rgb(199, 146, 234)',
+    fontStyle: 'italic',
+  },
+  'attr-name': {
+    color: 'rgb(173, 219, 103)',
+    fontStyle: 'italic',
+  },
+  string: {
+    color: 'rgb(173, 219, 103)',
+  },
+  char: {
+    color: 'rgb(130, 170, 255)',
+  },
+  builtin: {
+    color: 'rgb(130, 170, 255)',
+  },
+  inserted: {
+    color: 'rgb(173, 219, 103)',
+    fontStyle: 'italic',
+  },
+  operator: {
+    color: 'rgb(127, 219, 202)',
+  },
+  entity: {
+    color: 'rgb(173, 219, 103)',
+    cursor: 'help',
+  },
+  url: {
+    color: 'rgb(173, 219, 103)',
+  },
+  '.language-css .token.string': {
+    color: 'rgb(173, 219, 103)',
+  },
+  '.style .token.string': {
+    color: 'rgb(173, 219, 103)',
+  },
+  variable: {
+    color: 'rgb(214, 222, 235)',
+  },
+  atrule: {
+    color: 'rgb(255, 203, 139)',
+  },
+  'attr-value': {
+    color: 'rgb(255, 203, 139)',
+  },
+  function: {
+    color: 'rgb(130, 170, 255)',
+  },
+  keyword: {
+    color: 'rgb(127, 219, 202)',
+  },
+  regex: {
+    color: 'rgb(214, 222, 235)',
+  },
+  important: {
+    color: 'rgb(214, 222, 235)',
+    fontWeight: 'bold',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  italic: {
+    fontStyle: 'italic',
+  },
+  'class-name': {
+    color: 'rgb(255, 203, 139)',
+  },
+};
+
+// pre[class*='language-']::-moz-selection,
+// pre[class*='language-'] ::-moz-selection,
+// code[class*='language-']::-moz-selection,
+// code[class*='language-'] ::-moz-selection {
+//     text-shadow: none;
+//     background: #011627;
+// }
+
+// pre[class*='language-']::selection,
+// pre[class*='language-'] ::selection,
+// code[class*='language-']::selection,
+// code[class*='language-'] ::selection {
+//     text-shadow: none;
+//     background: #011627;
+// }
+
+// @media print {
+//     code[class*='language-'],
+//     pre[class*='language-'] {
+//         text-shadow: none;
+//     }
+// }
